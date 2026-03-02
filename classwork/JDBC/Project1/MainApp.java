6f13ea19d12d80cb5b59a0bdb94f5dd4fc8f05ea
Khushi Ranjan <155061852+Khushi-Ranjan@users.noreply.github.com>
2026-03-02 11:25:05 +0530

    Add MainApp class with employee insertion logic

---
+++ classwork/JDBC/Project1/MainApp.java
@@ -0,0 +1,10 @@
+package packing;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        EmployeeService service = new EmployeeService();
+
+        service.insertEmployee(123434,"Gungun", 8111.00);
+    }
+}
